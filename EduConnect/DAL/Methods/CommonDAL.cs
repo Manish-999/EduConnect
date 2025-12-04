038c63244b08f8460609d444356173dea0dd89e2
manishpandey4 <manish.pandey@zigram.tech>
2025-12-04 23:53:20 +0530

    first commit

---
+++ EduConnect/DAL/Methods/CommonDAL.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace DAL.Methods
+{
+    public class CommonDAL
+    {
+        public CommonDAL()
+        {
+
+        }
+        public async Task<IEnumerable<T>> GetAllDataFromDbByFunNameAndClientName<T>(string funName, int projectId)
+        {
+            string query = $"SELECT * FROM {funName}('{projectId}')";
+            try
+            {
+                using (var connection = new NpgsqlConnection(""))
+                {
+                    connection.Open();
+                    var projectWiseParameters = await connection.QueryAsync<T>(query, null);
+
+                    return projectWiseParameters;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+    }
+}
